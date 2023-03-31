6ea81828eb77577e1ffd0a32caf4873503327b26
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-03-31 22:31:39 +0700

    Add files via upload

---
+++ pertemuan3/latihan3/File Projects/Belah Ketupat/Belah Ketupat/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim Diagonal1, Diagonal2, sisi, luas, keliling As Integer
+
+        Diagonal1 = Val(txtD1.Text)
+        Diagonal2 = Val(txtD2.Text)
+        sisi = Val(txtSisi.Text)
+
+        luas = 1 / 2 * (Diagonal1 * Diagonal2)
+        keliling = sisi * 4
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtJari_TextChanged(sender As Object, e As EventArgs) Handles txtSisi.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
