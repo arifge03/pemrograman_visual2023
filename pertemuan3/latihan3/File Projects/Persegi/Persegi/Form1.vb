d514ba628497f9650378f2c7e09f6067d492ad1c
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-01 21:24:47 +0700

    Add files via upload

---
+++ pertemuan3/latihan3/File Projects/Persegi/Persegi/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim sisi, luas, keliling As Integer
+
+        sisi = Val(txtSisi.Text)
+
+        luas = sisi * sisi
+        keliling = sisi * 4
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtJari_TextChanged(sender As Object, e As EventArgs) Handles txtSisi.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
