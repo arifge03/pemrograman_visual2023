e4038bfa06d5bfb2e29b4a87497ca0c92b218df8
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-01 21:58:03 +0700

    Add files via upload

---
+++ pertemuan3/latihan3/File Projects/Segitiga/Segitiga/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim alas, tinggi, sisia, sisib, sisic, luas, keliling As Integer
+
+        alas = Val(txtAlas.Text)
+        tinggi = Val(txtTinggi.Text)
+        sisia = Val(txtSisia.Text)
+        sisib = Val(txtSisib.Text)
+        sisic = Val(txtSisic.Text)
+
+        luas = 1 / 2 * alas * tinggi
+        keliling = sisia + sisib + sisic
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtJari_TextChanged(sender As Object, e As EventArgs) Handles txtSisia.TextChanged
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
