9064dc5b30bf172565352947c11b299764a804f9
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-02 22:09:24 +0700

    Add files via upload

---
+++ pertemuan3/praktikum3/File Projects/Bola/Bola/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private Function HitungVolume(jari As Integer) As Integer
+        Dim volume As Integer
+        volume = 4 / 3 * Math.PI * Math.Pow(jari, 3)
+        Return volume
+    End Function
+    Private Function HitungLuasPermukaan(jari As Integer) As Integer
+        Dim luas_permukaan As Integer
+        luas_permukaan = 4 * Math.PI * Math.Pow(jari, 2)
+        Return luas_permukaan
+    End Function
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim jari, volume, luas_permukaan As Integer
+
+        jari = Val(txtJari.Text)
+
+        volume = HitungVolume(jari)
+        luas_permukaan = HitungLuasPermukaan(jari)
+
+        txtVolume.Text = Str(volume)
+        txtLuasPermukaan.Text = Str(luas_permukaan)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtJari_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub txtKeliling_TextChanged(sender As Object, e As EventArgs) Handles txtLuasPermukaan.TextChanged
+
+    End Sub
+End Class
