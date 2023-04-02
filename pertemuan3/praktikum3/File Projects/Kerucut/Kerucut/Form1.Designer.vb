f4d1e5c34f9bdece8330dc27aed08dc14a26158a
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-02 22:15:44 +0700

    Add files via upload

---
+++ pertemuan3/praktikum3/File Projects/Kerucut/Kerucut/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtVolume = New System.Windows.Forms.TextBox()
+        Me.txtLuasPermukaan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtJari = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTinggi = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.Color.Lime
+        Me.btnHitung.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.Location = New System.Drawing.Point(435, 281)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(128, 36)
+        Me.btnHitung.TabIndex = 1
+        Me.btnHitung.Text = "Hitung" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'txtVolume
+        '
+        Me.txtVolume.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtVolume.Location = New System.Drawing.Point(411, 337)
+        Me.txtVolume.Name = "txtVolume"
+        Me.txtVolume.Size = New System.Drawing.Size(191, 29)
+        Me.txtVolume.TabIndex = 2
+        '
+        'txtLuasPermukaan
+        '
+        Me.txtLuasPermukaan.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLuasPermukaan.Location = New System.Drawing.Point(411, 392)
+        Me.txtLuasPermukaan.Name = "txtLuasPermukaan"
+        Me.txtLuasPermukaan.Size = New System.Drawing.Size(191, 29)
+        Me.txtLuasPermukaan.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Lime
+        Me.Label2.Location = New System.Drawing.Point(210, 346)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 23)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Volume"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(207, 398)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(174, 23)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Luas Permukaan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("MS Reference Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(80, 65)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(767, 29)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Program Menghitung Volume dan Luas Permukaan Kerucut" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Lime
+        Me.Label5.Location = New System.Drawing.Point(207, 180)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(89, 23)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Jari-jari"
+        '
+        'txtJari
+        '
+        Me.txtJari.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtJari.Location = New System.Drawing.Point(411, 174)
+        Me.txtJari.Name = "txtJari"
+        Me.txtJari.Size = New System.Drawing.Size(191, 29)
+        Me.txtJari.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Lime
+        Me.Label6.Location = New System.Drawing.Point(207, 238)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(70, 23)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Tinggi"
+        '
+        'txtTinggi
+        '
+        Me.txtTinggi.Font = New System.Drawing.Font("MS Reference Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTinggi.Location = New System.Drawing.Point(411, 232)
+        Me.txtTinggi.Name = "txtTinggi"
+        Me.txtTinggi.Size = New System.Drawing.Size(191, 29)
+        Me.txtTinggi.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(904, 509)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtTinggi)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtJari)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtLuasPermukaan)
+        Me.Controls.Add(Me.txtVolume)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Kerucut"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtVolume As TextBox
+    Friend WithEvents txtLuasPermukaan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtJari As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTinggi As TextBox
+End Class
---
+++ pertemuan3/praktikum3/File Projects/Kerucut/Kerucut/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Function HitungVolume(jari As Integer, tinggi As Integer) As Integer
+        Dim volume As Integer
+        volume = 1 / 3 * Math.PI * Math.Pow(jari, 2) * tinggi
+        Return volume
+    End Function
+    Private Function HitungLuasPermukaan(jari As Integer, tinggi As Integer) As Integer
+        Dim luas_permukaan As Integer
+        luas_permukaan = Math.PI * jari * (jari + Math.Sqrt(Math.Pow(jari, 2) + Math.Pow(tinggi, 2)))
+        Return luas_permukaan
+    End Function
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim jari, tinggi, volume, luas_permukaan As Integer
+
+        jari = Val(txtJari.Text)
+        tinggi = Val(txtTinggi.Text)
+
+        volume = HitungVolume(jari, tinggi)
+        luas_permukaan = HitungLuasPermukaan(jari, tinggi)
+
+        txtVolume.Text = Str(volume)
+        txtLuasPermukaan.Text = Str(luas_permukaan)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtJari_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub txtKeliling_TextChanged(sender As Object, e As EventArgs) Handles txtLuasPermukaan.TextChanged
+
+    End Sub
+End Class
