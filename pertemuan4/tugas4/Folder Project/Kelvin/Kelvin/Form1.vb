775b366378e76d494aa1a7b235fa85a30eaee33a
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-06 22:32:50 +0700

    Add files via upload

---
+++ pertemuan4/tugas4/Folder Project/Kelvin/Kelvin/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim C, F, R, K As Decimal
+        K = CDec(txtKelvin.Text)
+        C = ToCelcius(K)
+        F = ToFahrenheit(K)
+        R = ToReamur(K)
+
+        txtCelcius.Text = Str(C)
+        txtFahrenheit.Text = Str(F)
+        txtReamur.Text = Str(R)
+    End Sub
+
+    Private Function ToCelcius(suhu As Decimal) As Decimal
+        Dim C As Decimal
+        C = suhu - 273
+        Return C
+    End Function
+
+    Private Function ToFahrenheit(suhu As Decimal) As Decimal
+        Dim F As Decimal
+        F = 4 / 5 * (suhu - 273)
+        Return F
+    End Function
+
+    Private Function ToReamur(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = 9 / 5 * (suhu - 273) + 32
+        Return K
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ pertemuan4/tugas4/Folder Project/Reamur/Reamur/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim C, F, R, K As Decimal
+        R = CDec(txtReamur.Text)
+        C = ToCelcius(R)
+        F = ToFahrenheit(R)
+        K = ToKelvin(R)
+
+        txtCelcius.Text = Str(C)
+        txtFahrenheit.Text = Str(F)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(suhu As Decimal) As Decimal
+        Dim C As Decimal
+        C = (5 / 4) * suhu
+        Return C
+    End Function
+
+    Private Function ToFahrenheit(suhu As Decimal) As Decimal
+        Dim F As Decimal
+        F = (9 / 4) * suhu + 32
+        Return F
+    End Function
+
+    Private Function ToKelvin(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = 5 / 4 * suhu + 273
+        Return K
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
