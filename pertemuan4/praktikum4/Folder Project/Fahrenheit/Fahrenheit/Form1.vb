4acdbdd091f2c9bbd182ebea1171d3fae553f8e7
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-06 22:29:57 +0700

    Add files via upload

---
+++ pertemuan4/praktikum4/Folder Project/Fahrenheit/Fahrenheit/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim C, F, R, K As Decimal
+        F = CDec(txtFahrenheit.Text)
+        C = ToCelcius(F)
+        R = ToReamur(F)
+        K = ToKelvin(F)
+
+        txtCelcius.Text = Str(C)
+        txtReamur.Text = Str(R)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(suhu As Decimal) As Decimal
+        Dim C As Decimal
+        C = 5 / 9 * (suhu - 32)
+        Return C
+    End Function
+
+    Private Function ToReamur(suhu As Decimal) As Decimal
+        Dim R As Decimal
+        R = 4 / 9 * (suhu - 32)
+        Return R
+    End Function
+
+    Private Function ToKelvin(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = 5 / 9 * (suhu - 32) + 273
+        Return K
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
