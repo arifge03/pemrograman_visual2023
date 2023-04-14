3fd2616976706b108b049c47ddfbb64114912ee6
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-04-14 07:27:49 +0700

    Add files via upload

---
+++ pertemuan5/tugas5/Folder Projects/Tugas5/Tugas5/Form1.Designer.vb
@@ -0,0 +1,358 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNIM = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtUTS = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtTugas = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtNilaiKehadiran = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtUAS = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtNilaiMutu = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtNilaiAkhir = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtProdiOutput = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtNamaOutput = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.txtNIMOutput = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNIM
+        '
+        Me.txtNIM.Location = New System.Drawing.Point(200, 120)
+        Me.txtNIM.Name = "txtNIM"
+        Me.txtNIM.Size = New System.Drawing.Size(432, 26)
+        Me.txtNIM.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NIM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 165)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nama"
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(200, 162)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(432, 26)
+        Me.txtNama.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(134, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(424, 32)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Aplikasi Penentuan Nilai Mutu"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(44, 206)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Prodi"
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(200, 203)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(432, 26)
+        Me.txtProdi.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(44, 329)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 20)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Nilai UTS"
+        '
+        'txtUTS
+        '
+        Me.txtUTS.Location = New System.Drawing.Point(200, 326)
+        Me.txtUTS.Name = "txtUTS"
+        Me.txtUTS.Size = New System.Drawing.Size(432, 26)
+        Me.txtUTS.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(44, 288)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(86, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Nilai Tugas"
+        '
+        'txtTugas
+        '
+        Me.txtTugas.Location = New System.Drawing.Point(200, 285)
+        Me.txtTugas.Name = "txtTugas"
+        Me.txtTugas.Size = New System.Drawing.Size(432, 26)
+        Me.txtTugas.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(44, 246)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(114, 20)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Nilai Kehadiran"
+        '
+        'txtNilaiKehadiran
+        '
+        Me.txtNilaiKehadiran.Location = New System.Drawing.Point(200, 243)
+        Me.txtNilaiKehadiran.Name = "txtNilaiKehadiran"
+        Me.txtNilaiKehadiran.Size = New System.Drawing.Size(432, 26)
+        Me.txtNilaiKehadiran.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(44, 374)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(76, 20)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Nilai UAS"
+        '
+        'txtUAS
+        '
+        Me.txtUAS.Location = New System.Drawing.Point(200, 371)
+        Me.txtUAS.Name = "txtUAS"
+        Me.txtUAS.Size = New System.Drawing.Size(432, 26)
+        Me.txtUAS.TabIndex = 14
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(44, 727)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(78, 20)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "Nilai Mutu"
+        '
+        'txtNilaiMutu
+        '
+        Me.txtNilaiMutu.Location = New System.Drawing.Point(200, 724)
+        Me.txtNilaiMutu.Name = "txtNilaiMutu"
+        Me.txtNilaiMutu.Size = New System.Drawing.Size(432, 26)
+        Me.txtNilaiMutu.TabIndex = 19
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(44, 682)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(78, 20)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Nilai Akhir"
+        '
+        'txtNilaiAkhir
+        '
+        Me.txtNilaiAkhir.Location = New System.Drawing.Point(200, 679)
+        Me.txtNilaiAkhir.Name = "txtNilaiAkhir"
+        Me.txtNilaiAkhir.Size = New System.Drawing.Size(432, 26)
+        Me.txtNilaiAkhir.TabIndex = 17
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(256, 415)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(317, 46)
+        Me.btnSubmit.TabIndex = 21
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(42, 503)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(310, 32)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "Hasil Penentuan Nilai"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(44, 636)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(45, 20)
+        Me.Label9.TabIndex = 28
+        Me.Label9.Text = "Prodi"
+        '
+        'txtProdiOutput
+        '
+        Me.txtProdiOutput.Location = New System.Drawing.Point(200, 633)
+        Me.txtProdiOutput.Name = "txtProdiOutput"
+        Me.txtProdiOutput.Size = New System.Drawing.Size(432, 26)
+        Me.txtProdiOutput.TabIndex = 27
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(44, 595)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(51, 20)
+        Me.Label13.TabIndex = 26
+        Me.Label13.Text = "Nama"
+        '
+        'txtNamaOutput
+        '
+        Me.txtNamaOutput.Location = New System.Drawing.Point(200, 592)
+        Me.txtNamaOutput.Name = "txtNamaOutput"
+        Me.txtNamaOutput.Size = New System.Drawing.Size(432, 26)
+        Me.txtNamaOutput.TabIndex = 25
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(44, 553)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(38, 20)
+        Me.Label14.TabIndex = 24
+        Me.Label14.Text = "NIM"
+        '
+        'txtNIMOutput
+        '
+        Me.txtNIMOutput.Location = New System.Drawing.Point(200, 550)
+        Me.txtNIMOutput.Name = "txtNIMOutput"
+        Me.txtNIMOutput.Size = New System.Drawing.Size(432, 26)
+        Me.txtNIMOutput.TabIndex = 23
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(44, 784)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(249, 20)
+        Me.Label15.TabIndex = 29
+        Me.Label15.Text = "Arif Gunawan_200511126_TI20D"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(697, 823)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtProdiOutput)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.txtNamaOutput)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.txtNIMOutput)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtNilaiMutu)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.txtNilaiAkhir)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtUAS)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtUTS)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtTugas)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtNilaiKehadiran)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNIM)
+        Me.MaximumSize = New System.Drawing.Size(719, 879)
+        Me.MinimumSize = New System.Drawing.Size(719, 879)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNIM As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtUTS As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtTugas As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtNilaiKehadiran As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtUAS As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtNilaiMutu As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txtNilaiAkhir As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtProdiOutput As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents txtNamaOutput As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents txtNIMOutput As TextBox
+    Friend WithEvents Label15 As Label
+End Class
---
+++ pertemuan5/tugas5/Folder Projects/Tugas5/Tugas5/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Function HitungNilaiAkhir(khd As Integer, tgs As Integer, uts As Integer, uas As Integer) As Integer
+        Dim nilaiAkhir As Integer
+        nilaiAkhir = (25 / 100 * khd) + (35 / 100 * tgs) + (20 / 100 * uts) + (20 / 100 * uas)
+        Return nilaiAkhir
+    End Function
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim nim, khd, tgs, uts, uas, nilaiAkhir As Integer
+        Dim nama, prodi As String
+
+        nim = Val(txtNIM.Text)
+        nama = Val(txtNama.Text)
+        prodi = Val(txtProdi.Text)
+        khd = Val(txtNilaiKehadiran.Text)
+        tgs = Val(txtTugas.Text)
+        uts = Val(txtUTS.Text)
+        uas = Val(txtUAS.Text)
+
+        nilaiAkhir = HitungNilaiAkhir(khd, tgs, uts, uas)
+
+        txtNIMOutput.Text = Str(nim)
+        txtNamaOutput.Text = txtNama.Text
+        txtProdiOutput.Text = txtProdi.Text
+
+        txtNilaiAkhir.Text = Str(nilaiAkhir)
+        If (nilaiAkhir >= 85) Then
+            txtNilaiMutu.Text = "A"
+
+        ElseIf (nilaiAkhir >= 75) Then
+            txtNilaiMutu.Text = "B"
+
+        ElseIf (nilaiAkhir >= 55) Then
+            txtNilaiMutu.Text = "C"
+
+        ElseIf (nilaiAkhir >= 30) Then
+            txtNilaiMutu.Text = "D"
+
+        Else
+            txtNilaiMutu.Text = "E"
+        End If
+    End Sub
+End Class
+
