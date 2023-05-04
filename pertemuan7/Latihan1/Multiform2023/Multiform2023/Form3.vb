00914199e17d4ccbf9ff33abdd20bb00eb5a7b97
arifge03 <103354145+arifge03@users.noreply.github.com>
2023-05-04 21:43:22 +0700

    Add files via upload

---
+++ pertemuan7/Latihan1/Multiform2023/Multiform2023/Form3.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form3
+    Private Sub F1(sender As Object, e As EventArgs) Handles Form1.Click
+        Dim F1 As New Form2
+        F1.ShowDialog()
+    End Sub
+    Private Sub F2(sender As Object, e As EventArgs) Handles Form2.Click
+        Dim F2 As New Form3
+        F2.ShowDialog()
+    End Sub
+    Private Sub F4(sender As Object, e As EventArgs) Handles Form4.Click
+        Dim F4 As New Form4
+        F4.ShowDialog()
+    End Sub
+    Private Sub F5(sender As Object, e As EventArgs) Handles Form5.Click
+        Dim F5 As New Form5
+        F5.ShowDialog()
+    End Sub
+End Class
---
+++ pertemuan7/Latihan2/appMenu/appMenu/Form3.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS Reference Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(233, 146)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 40)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Form-3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 273)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(574, 40)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Arif Gunawan - 200511126 -TI20D"
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 341)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form3"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
